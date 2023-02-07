562e1895ddd36b37adc059a599bebe7f4d1d5c12
Colton Acosta <cacost12@asu.edu>
2023-02-07 09:56:39 -0700

    added init.c mcu initialization

--- app/blink/main.c
+++ app/blink/main.c
@@ -20,10 +20,12 @@ Project Includes
 ------------------------------------------------------------------------------*/
 
 /* Pin definitions and main prototypes */
-#include "main.h"
 #include "sdr_pin_defines_A0007.h"
+#include "main.h"
+#include "init.h"
 
 /* SDR Modules */
+#include "led.h"
 
 
 /*------------------------------------------------------------------------------
@@ -37,13 +39,6 @@ Typedefs
 
 
 /*------------------------------------------------------------------------------
-Function prototypes                                                          
-------------------------------------------------------------------------------*/
-void        SystemClock_Config( void );
-static void GPIO_Init         ( void );
-
-
-/*------------------------------------------------------------------------------
 Application entry point                                                      
 ------------------------------------------------------------------------------*/
 int main
@@ -54,7 +49,7 @@ int main
 /*------------------------------------------------------------------------------
 Local Variables
 ------------------------------------------------------------------------------*/
-uint16_t rgb_led_bitmask = 0x0000;
+
 
 /*------------------------------------------------------------------------------
 MCU Initialization                                                                  
@@ -71,187 +66,23 @@ while (1)
 	/* Loop over 8 basic rgb led settings */
 	for ( uint8_t i = 0; i < 8; ++i )
 		{
-		/* Initialize bit mask */
-		rgb_led_bitmask = 0x0000;
-		
-		/* Determine the contents of the rgb register */
-		switch ( i )
+		/* Start with LED off */
+		if ( i == 0 )
 			{
-			case 0:
-				{
-				rgb_led_bitmask = 0x0000;
-				break;
-                }
-			
-			case 1:
-				{
-				rgb_led_bitmask = STATUS_B_PIN;
-				break;
-                }
+			led_reset();
+			}
+		else
+			{
+			/* Cycle through solid RGB colors */
+			led_set_color( i );
+			}
 
-			case 2:
-				{
-				rgb_led_bitmask = STATUS_G_PIN;
-				break;
-                }
-
-			case 3:
-				{
-				rgb_led_bitmask = STATUS_R_PIN;
-				break;
-                }
-
-			case 4:
-				{
-				rgb_led_bitmask = STATUS_G_PIN |
-                                  STATUS_B_PIN;
-				break;
-                }
-
-			case 5:
-				{
-				rgb_led_bitmask = STATUS_R_PIN |
-                                  STATUS_B_PIN;
-				break;
-                }
-
-			case 6:
-				{
-				rgb_led_bitmask = STATUS_R_PIN |
-                                  STATUS_G_PIN;
-				break;
-                }
-
-			case 7:
-				{
-				rgb_led_bitmask = STATUS_R_PIN |
-	                              STATUS_B_PIN |
-                                  STATUS_G_PIN;
-				break;
-                }
-			} /* switch case */
-
-		/* Update the LED */
-		HAL_GPIO_WritePin( STATUS_GPIO_PORT,
-                           STATUS_R_PIN |
-                           STATUS_G_PIN |
-                           STATUS_B_PIN    ,
-                           GPIO_PIN_SET );
-		HAL_GPIO_WritePin( STATUS_GPIO_PORT, rgb_led_bitmask, GPIO_PIN_RESET );
+		/* Hold till next time period */
 		HAL_Delay( 500 );
         }
 	}
 
 } /* main.c */
-
-
-/*******************************************************************************
-*                                                                              *
-* PROCEDURE:                                                                   * 
-*       SystemClock_Config                                                     *
-*                                                                              *
-* DESCRIPTION:                                                                 * 
-*       Initializes the microcontroller clock. Enables peripheral clocks and   *
-*       sets prescalers                                                        *
-*                                                                              *
-*******************************************************************************/
-void SystemClock_Config
-	(
-	void
-	)
-{
-/* Init structs */
-RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-/* Supply configuration update enable */
-HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
-
-/* Configure the main internal regulator output voltage */
-__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
-
-while( !__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY) ) 
-	{
-	/* Wait for PWR_FLAG_VOSRDY flag */
-	}
-
-/* Initializes the RCC Oscillators according to the specified parameters 
-   in the RCC_OscInitTypeDef structure. */
-RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
-RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
-RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
-RCC_OscInitStruct.PLL.PLLM       = 2;
-RCC_OscInitStruct.PLL.PLLN       = 16;
-RCC_OscInitStruct.PLL.PLLP       = 2;
-RCC_OscInitStruct.PLL.PLLQ       = 2;
-RCC_OscInitStruct.PLL.PLLR       = 2;
-RCC_OscInitStruct.PLL.PLLRGE     = RCC_PLL1VCIRANGE_3;
-RCC_OscInitStruct.PLL.PLLVCOSEL  = RCC_PLL1VCOWIDE;
-RCC_OscInitStruct.PLL.PLLFRACN   = 0;
-if ( HAL_RCC_OscConfig( &RCC_OscInitStruct ) != HAL_OK )
-	{
-	Error_Handler();
-	}
-
-/* Initializes the CPU, AHB and APB buses clocks */
-RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK    |
-                                   RCC_CLOCKTYPE_SYSCLK  |
-								   RCC_CLOCKTYPE_PCLK1   |
-								   RCC_CLOCKTYPE_PCLK2   |
-								   RCC_CLOCKTYPE_D3PCLK1 |
-								   RCC_CLOCKTYPE_D1PCLK1;
-RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-RCC_ClkInitStruct.SYSCLKDivider  = RCC_SYSCLK_DIV1;
-RCC_ClkInitStruct.AHBCLKDivider  = RCC_HCLK_DIV1;
-RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
-RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
-RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
-RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
-if ( HAL_RCC_ClockConfig( &RCC_ClkInitStruct, FLASH_LATENCY_1 ) != HAL_OK )
-	{
-	Error_Handler();
-	}
-} /* SystemClock_Config */
-
-
-/*******************************************************************************
-*                                                                              *
-* PROCEDURE:                                                                   *
-*       GPIO_Init                                                              * 
-*                                                                              *
-* DESCRIPTION:                                                                 * 
-*       Initializes all GPIO pins and sets alternate functions                 *
-*                                                                              *
-*******************************************************************************/
-static void GPIO_Init
-	(
-	void
-	)
-{
-/* Init Struct */
-GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-/* GPIO Ports Clock Enable */
-__HAL_RCC_GPIOA_CLK_ENABLE();
-
-/*Configure GPIO pin Output Level */
-HAL_GPIO_WritePin( STATUS_GPIO_PORT, 
-                   STATUS_B_PIN |
-                   STATUS_G_PIN |
-                   STATUS_R_PIN    , 
-                   GPIO_PIN_RESET );
-
-/*Configure GPIO pins : PA9 PA10 PA11 */
-GPIO_InitStruct.Pin   = STATUS_B_PIN |
-                        STATUS_G_PIN |
-					    STATUS_R_PIN;
-GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_OD;
-GPIO_InitStruct.Pull  = GPIO_NOPULL;
-GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-HAL_GPIO_Init( STATUS_GPIO_PORT, &GPIO_InitStruct);
-
-} /* GPIO_Init */
 
 
 /*******************************************************************************
